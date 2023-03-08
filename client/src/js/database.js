d3ef41ad81bcf870b624527eb577f9603fce3303
luksvrd <ljvy9b@gmail.com>
2023-03-07 21:10:18 -0600

    refactor to fix db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,22 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method that takes some content and adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({ jate: content });
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log("Saved in database", result);
+  console.log('🚀 - data saved to the database', result.value);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Method that gets content from the IndexedDB database using the idb module
 export const getDb = async () => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
-  const request = store.getAll();
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
   const result = await request;
-  console.log(result);
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
 };
+
+initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,20 +23,24 @@ module.exports = () => {
       // index.html will be used to run the app on desktop.
       new HtmlWebpackPlugin({
         template: './index.html',
-        filename: 'index.html',
-        chunks: ['main'],
+        title: 'J.A.T.E'
+      }),
+      // Workbox plugin - InjectManifest will generate a service worker to inject into index.html file.
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
       }),
       // WebpackPwaManifest will generate a manifest.json file for app. (how the app will behave on mobile)
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Just Another Text Editor",
-        short_name: "JATE",
-        description: "Text Editor with offline capabilities using IndexedDB",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
-        start_url: "/",
-        publicPath: "/",
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve("src/images/logo.png"),
@@ -44,11 +48,6 @@ module.exports = () => {
             destination: path.join("assets", "icons"),
           },
         ],
-      }),
-      // Workbox plugin - InjectManifest will generate a service worker to inject into index.html file.
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js'
       }),
     ],
     // CSS loaders and babel
